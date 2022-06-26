0a3b38ec6533f52c4768ef009f31f25171b062f8
nezrinabbaszade2@gmail.com <nezrinabbaszade2@gmail.com>
2022-06-26 13:42:35 +0400

    changed name to surname

--- M126Git/User.cs
+++ M126Git/User.cs
@@ -9,6 +9,6 @@ namespace M126Git
     public class User
     {
         public int Id { get; set; } 
-        public string Name { get; set; }    
+        public string Surname { get; set; }    
     }
 }
