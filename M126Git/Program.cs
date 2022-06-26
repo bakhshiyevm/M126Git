60f9f3b1c69450c2922276aee150736398a04aef
BakhshiyevM <mbakhshiyev@pasha-insurance.az>
2022-06-26 13:50:31 +0400

    good dev

--- M126Git/Program.cs
+++ M126Git/Program.cs
@@ -6,7 +6,7 @@ namespace M126Git
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("One day u will became developers");
+            Console.WriteLine("One day u will became good developers");
         }
 
     }
