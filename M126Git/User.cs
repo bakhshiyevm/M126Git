4391a2628e190b452ee25b086c940e46bd8a9e3d
BakhshiyevM <mbakhshiyev@pasha-insurance.az>
2022-06-30 20:19:39 +0400

    gender to name

--- M126Git/User.cs
+++ M126Git/User.cs
@@ -10,7 +10,7 @@ namespace M126Git
     {
         public int Id { get; set; } 
         public string Surname { get; set; }
-        public string Gender { get; set; }
+        public string Name { get; set; }
         public int Age { get; set; }
     }
 }
